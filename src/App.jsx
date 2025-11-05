e8e5ed1c39dcfddd385dfa15f0dc46a8772d3c81
faizzaidi23 <786faizzaidi@gmail.com>
2025-11-06 00:25:44 +0530

    feat:Started the project and made some basic files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import WaterLevelDashboard from './components/WaterLevelDashboard'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <WaterLevelDashboard />
+    </div>
+  )
+}
+
+export default App
+
